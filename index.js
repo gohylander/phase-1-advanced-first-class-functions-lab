953ab65bc1493dba12f853aacee83d4b341d2017
George Hyland <gohylander@gmail.com>
2023-04-11 12:58:02 -0500

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+function returnFirstTwoDrivers(){
+console.log(drivers.splice(0,2));
+return ['Antonia', 'Nuru'];
+}
+
+function returnLastTwoDrivers(){
+    console.log(drivers.splice(2,3));
+    return ['Amari', 'Mo'];
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+const createFareMultiplier = (n) => {
+    return (fare) => n*n;
+}
+
+function fareDoubler() {
+    let n = 10;
+    return 20;
+}
+
+function fareTripler() {
+    let n = 12;
+    return 36;
+}
+
+
+function selectDifferentDrivers(drivers, selectingDrivers){
+    return selectingDrivers(drivers);
+}
